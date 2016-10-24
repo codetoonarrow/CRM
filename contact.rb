5681de3536476c732dbe511e9f6da4296400e176
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-10-24 18:06:05 -0400

    Can search by first/last name + email + note

--- contact.rb
+++ contact.rb
@@ -43,27 +43,39 @@ class Contact
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
   def update(x, new_value)
-    if x == "first name"
-      contact.first_name = new_value
+    if
+      x == "first name"
+      self.first_name == new_value
     elsif
       x == "last name"
-      contact.last_name = new_value
+      self.last_name == new_value
     elsif
-      x == "email"
-      contact.email = new_value
+      x  == "email"
+      self.email == new_value
     elsif
       x == "note"
-      contact.note = new_value
+      self.note == new_value
     end
-
   end
 
   # This method should work similarly to the find method above
   # but it should allow you to search for a contact using attributes other than id
   # by specifying both the name of the attribute and the value
   # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-  def self.find_by(k, v)
 
+  def self.find_by(attr_name, value)
+    @@contacts.each do |contact|
+      if attr_name == "first name" && value == contact.first_name
+        return contact
+      elsif attr_name == "last name" && value == contact.last_name
+        return contact
+      elsif attr_name == "email" && value == contact.email
+        return contact
+      elsif attr_name == "note" && value == contact.note
+        return contact
+      end
+    end
+    return nil
   end
 
   # This method should delete all of the contacts
@@ -79,6 +91,7 @@ class Contact
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
   def delete
+    @@contacts.delete
 
   end
 
